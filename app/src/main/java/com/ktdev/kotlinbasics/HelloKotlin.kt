8b2e5d0b84e9a447ac3f6a5668053c0542f5c8ed
Kshitij Thakre <kshitijthakre@Kshitijs-MacBook-Air.local>
2026-02-13 09:57:03 +0530

    Intial commit

---
+++ app/src/main/java/com/ktdev/kotlinbasics/HelloKotlin.kt
@@ -0,0 +1,24 @@
+package com.ktdev.kotlinbasics
+
+//In kotlin "fun main(){}" is the entry point of a kotlin program.
+//It serves as starting point of execution when you run a Kotlin App.
+
+fun main (){
+
+    /*A variable is named storage location in which you can store and manipulate data.
+    * Step to create variables:
+    * - Variable Declaration
+    * - Type Inference
+    * - Assigning Values
+    */
+
+    //Mutable variables
+    var  myAge : Int = 26
+    //Immutable variables
+    val  pi : Double = 3.1419
+
+
+    println("My age is:$myAge")
+
+
+}
